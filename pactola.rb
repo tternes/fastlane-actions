6eb44fa4fe7f5716148fc6b6e4a9f8b32ef7cfd0
Thaddeus Ternes <thaddeus@bluetoo.co>
2016-01-03 15:23:27 -0600

    Initial Pactola.io action

---
+++ pactola.rb
@@ -0,0 +1,81 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      PACTOLA_CUSTOM_VALUE = :PACTOLA_CUSTOM_VALUE
+    end
+
+    class PactolaAction < Action
+      def self.run(params)
+        
+        require "json"
+        require "net/http"
+        require "uri"
+
+        Helper.log.info "Using API Token: #{params[:device_token]}"
+
+        device_url = "https://pactola.io/#{params[:device_token]}/notification"
+        Helper.log.debug "device_url: #{device_url}"
+
+        uri = URI.parse(device_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.body = {title: "#{params[:title]}", message: "#{params[:message]}"}.to_json
+        response = http.request(request)
+
+        if response.code == "202"
+          Helper.log.info "Service returned 202 successfully"
+        else
+          Helper.log.error "Status code: " + response.code
+          response.each_header do |header_name, header_value|
+            Helper.log.error header_name + ":" + header_value
+          end
+          Helper.log.error response.body
+
+          throw "unexpected response from pactola.io"
+        end
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Sends messages to Pactola.io"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :device_token,
+                                       env_name: "PACTOLA_DEVICE_TOKEN",
+                                       description: "Device Token for Pactola",
+                                       verify_block: proc do |value|
+                                          raise "No device token for Pactola given, pass using `device_token: 'token'`".red unless (value and not value.empty?)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :title,
+                                       env_name: "PACTOLA_TITLE",
+                                       description: "Title for Pactola notification",
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                          raise "No notification title for Pactola given, pass using `title: 'value'`".red unless (value and not value.empty?)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :message,
+                                       env_name: "PACTOLA_MESSAGE",
+                                       description: "Message for Pactola notification",
+                                       verify_block: proc do |value|
+                                          raise "No notification message for Pactola given, pass using `message: 'value'`".red unless (value and not value.empty?)
+                                       end)
+        ]
+      end
+
+      def self.authors
+        ["thaddeus"]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
